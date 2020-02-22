72cb584b34515f94c468948ffccaafde212683e2
jttc98 <jttc98@csu.fullerton.edu>
2020-02-22 10:56:38 -0800

    First draft of treeview

--- in_progress_list.py
+++ in_progress_list.py
@@ -1,41 +1,50 @@
-from tksheet import Sheet #Needed to work with spreadsheets
 from tkinter import *
+import tkinter as tk
+from tkinter import ttk
 import mysql.connector
-import sys
 
 # In Progress List
 
-##class window(tk.Tk):
-##    # Declare a constructor containing the window specs
-##    def __init__(self):
-##        tk.Tk.__init__(self)
-##        self.grid_columnconfigure(0, weight=1)
-##        self.grid_rowconfigure(0, weight=1)
-##        self.sheet_demo = Sheet(self,
-##                                height=600,
-##                                width=1500)
-##        self.sheet_demo.grid(row=0, column=0, sticky="nswe")
+class pyTree:
+    def __init__(self,master):
+        self.master=master
+        self.tree=ttk.Treeview(master, column=("column", "column1",
+                                             "column2", "column3")) #Needed to create the new tree
+        master.title("Work In Progress")
 
-win = Tk()
-win.geometry("800x600") #Configure the width by height size
-win.title("Work In Progress")
+        # Create the columns and headings below
+        self.tree.column("#0", minwidth=0, width=0)
+        self.tree.heading("#1", text="Work #")
+        self.tree.column("#1", minwidth=0, width=100)
+        self.tree.heading("#2", text="Status")
+        self.tree.column("#2", minwidth=0, width=200)
+        self.tree.heading("#3", text="Days Until Shipment")
+        self.tree.column("#3", minwidth=0, width=150)
 
-# Create the label widgets below=
-lWorkNum = Label(win, text = "Work #", font = ("arial", 12, "bold"))
-lStatus = Label(win, text = "Status", font = ("arial", 12, "bold"))
-lDaysLeft = Label(win, text = "Days Until Shipment", font = ("arial", 12, "bold"))
-lTitle = Label(win, text = "Work In Progress", font = ("arial", 18, "bold"))
-lSearch = Label(win, text = "Search", font = ("arial", 13, "bold"))
+        self.tree.configure(height=20)
+        self.tree.grid()
+        self.transient(self.master)
+        self.focus_force()
+        self.grab_set()
 
-# Create the entry widget below
-eSearch = Entry(win)
+### Create the label widgets below
+##lWorkNum = Label(win, text = "Work #", font = ("arial", 12, "bold"))
+##lStatus = Label(win, text = "Status", font = ("arial", 12, "bold"))
+##lDaysLeft = Label(win, text = "Days Until Shipment", font = ("arial", 12, "bold"))
+##lTitle = Label(win, text = "Work In Progress", font = ("arial", 18, "bold"))
+##lSearch = Label(win, text = "Search", font = ("arial", 13, "bold"))
+##
+### Create the entry widget below
+##eSearch = Entry(win)
+##
+### Now place all the parts onto the window
+##lTitle.place(x=240, y=10)
+##lSearch.place(x=570, y=10)
+##eSearch.place(x=640, y=10)
+##lWorkNum.place(x=100, y=90)
+##lStatus.place(x=300, y=90)
+##lDaysLeft.place(x=500, y=90)
 
-# Now place all the parts onto the window
-lTitle.place(x=240, y=10)
-lSearch.place(x=570, y=10)
-eSearch.place(x=640, y=10)
-lWorkNum.place(x=100, y=90)
-lStatus.place(x=300, y=90)
-lDaysLeft.place(x=500, y=90)
-
-win.mainloop()
+window = Tk()
+main = pyTree(window)
+main.mainloop()

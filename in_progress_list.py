458ca337690b7ce3e9790c0ecbccca75a8e54381
jttc98 <jttc98@csu.fullerton.edu>
2020-02-21 18:37:12 -0800

    Update in_progress_list.py

--- in_progress_list.py
+++ in_progress_list.py
@@ -1,43 +1,41 @@
 from tksheet import Sheet #Needed to work with spreadsheets
-import tkinter as tk
+from tkinter import *
 import mysql.connector
 import sys
 
 # In Progress List
-# In order to work with tksheet, we'll need to create a class.
 
-# Create the class below
-class window(tk.Tk):
-    # Declare a constructor containing the window specs
-    def __init__(self):
-        tk.Tk.__init__(self)
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
-        self.geometry("800x600") #Configure the width by height size
-        self.title("In Progress")
-        self.sheet_demo = Sheet(self,
-                                height=600,
-                                width=1500)
-        self.sheet_demo.grid(row=0, column=0, sticky="nswe")
+##class window(tk.Tk):
+##    # Declare a constructor containing the window specs
+##    def __init__(self):
+##        tk.Tk.__init__(self)
+##        self.grid_columnconfigure(0, weight=1)
+##        self.grid_rowconfigure(0, weight=1)
+##        self.sheet_demo = Sheet(self,
+##                                height=600,
+##                                width=1500)
+##        self.sheet_demo.grid(row=0, column=0, sticky="nswe")
 
-### Create the label widgets below
-##lBlank = tk.Label(win, text= '                         ')
-##lWorkNum = tk.Label(win, text = "Work #")
-##lStatus = tk.Label(win, text = "Status")
-##lDaysLeft = tk.Label(win, text = "Days Until Shipment")
-##lTitle = tk.Label(win, text = 'Work In Progress')
-##lSearch = tk.Label(win, text = 'Search')
-##
-### Create the entry widget below
-##eSearch = tk.Entry(win)
-##
-### Now place all the parts in the GUI
-##lTitle.grid(row=0, column=1, pady=3)
-##lSearch.grid(row=0, column=2, pady=3)
-##eSearch.grid(row=0, column=3, pady=3)
-##lWorkNum.grid(row=1, column=1, pady=3)
-##lStatus.grid(row=1, column=2, pady=3)
-##lDaysLeft.grid(row=1, column=3, pady=3)
+win = Tk()
+win.geometry("800x600") #Configure the width by height size
+win.title("Work In Progress")
 
-win = window()
+# Create the label widgets below=
+lWorkNum = Label(win, text = "Work #", font = ("arial", 12, "bold"))
+lStatus = Label(win, text = "Status", font = ("arial", 12, "bold"))
+lDaysLeft = Label(win, text = "Days Until Shipment", font = ("arial", 12, "bold"))
+lTitle = Label(win, text = "Work In Progress", font = ("arial", 18, "bold"))
+lSearch = Label(win, text = "Search", font = ("arial", 13, "bold"))
+
+# Create the entry widget below
+eSearch = Entry(win)
+
+# Now place all the parts onto the window
+lTitle.place(x=240, y=10)
+lSearch.place(x=570, y=10)
+eSearch.place(x=640, y=10)
+lWorkNum.place(x=100, y=90)
+lStatus.place(x=300, y=90)
+lDaysLeft.place(x=500, y=90)
+
 win.mainloop()

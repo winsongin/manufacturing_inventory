77fec3c0072c67e42347f123e9228d50e79a7e66
jttc98 <jttc98@csu.fullerton.edu>
2020-02-21 17:36:11 -0800

    First draft of tksheet
    I also commented out the widgets for now. For the tksheet grid, we want to have the list of entries (that is, the work number, status, and days until shipment) included after the first two rows of the spreadsheet because the first row contains the title and the search while the second row will have the section titles for the work number, status, and days left respectively. Let me know what you guys think about this plan I came up with.

--- in_progress_list.py
+++ in_progress_list.py
@@ -1,39 +1,43 @@
+from tksheet import Sheet #Needed to work with spreadsheets
 import tkinter as tk
 import mysql.connector
 import sys
 
 # In Progress List
+# In order to work with tksheet, we'll need to create a class.
 
-# Create the window first
-win = tk.Tk()
-win.geometry("450x350") #Configure the width by height size
-mainHome = tk.Frame(win)
+# Create the class below
+class window(tk.Tk):
+    # Declare a constructor containing the window specs
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        self.geometry("800x600") #Configure the width by height size
+        self.title("In Progress")
+        self.sheet_demo = Sheet(self,
+                                height=600,
+                                width=1500)
+        self.sheet_demo.grid(row=0, column=0, sticky="nswe")
 
-# Create the label widgets below
-lBlank = tk.Label(win, text= '                         ')
-lWorkNum = tk.Label(win, text = "Work #")
-lStatus = tk.Label(win, text = "Status")
-lDaysLeft = tk.Label(win, text = "Days Until Shipment")
-lTitle = tk.Label(win, text = 'Work In Progress')
-lSearch = tk.Label(win, text = 'Search')
+### Create the label widgets below
+##lBlank = tk.Label(win, text= '                         ')
+##lWorkNum = tk.Label(win, text = "Work #")
+##lStatus = tk.Label(win, text = "Status")
+##lDaysLeft = tk.Label(win, text = "Days Until Shipment")
+##lTitle = tk.Label(win, text = 'Work In Progress')
+##lSearch = tk.Label(win, text = 'Search')
+##
+### Create the entry widget below
+##eSearch = tk.Entry(win)
+##
+### Now place all the parts in the GUI
+##lTitle.grid(row=0, column=1, pady=3)
+##lSearch.grid(row=0, column=2, pady=3)
+##eSearch.grid(row=0, column=3, pady=3)
+##lWorkNum.grid(row=1, column=1, pady=3)
+##lStatus.grid(row=1, column=2, pady=3)
+##lDaysLeft.grid(row=1, column=3, pady=3)
 
-# Create the entry widget below
-eSearch = tk.Entry(win)
-
-# Create the text widgets below
-tWorkNum = tk.Text(win, height=30, width=10, state='disabled')
-tStatus = tk.Text(win, height=30, width=10, state='disabled')
-tDaysLeft = tk.Text(win, height=30, width=10, state='disabled')
-
-# Now place all the parts in the GUI
-lTitle.grid(row=0, column=1, pady=3)
-lSearch.grid(row=0, column=2, pady=3)
-eSearch.grid(row=0, column=3, pady=3)
-lWorkNum.grid(row=1, column=0, pady=3)
-lStatus.grid(row=1, column=1, pady=3)
-lDaysLeft.grid(row=1, column=2, pady=3)
-tWorkNum.grid(row=2, column=0, padx=5)
-tStatus.grid(row=2, column=1, padx=5)
-tDaysLeft.grid(row=2, column=2, padx=5)
-
+win = window()
 win.mainloop()

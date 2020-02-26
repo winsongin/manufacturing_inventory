f4ad4c273542ab31c36d0f816b3af419b9784cab
jttc98 <jttc98@csu.fullerton.edu>
2020-02-26 09:38:20 -0800

    Deleted an unused column from TreeView

--- in_progress_list.py
+++ in_progress_list.py
@@ -8,8 +8,7 @@ import mysql.connector
 class in_progress:
     def __init__(self,master):
         self.master=master
-        self.tree=ttk.Treeview(master, column=("column", "column1",
-                                             "column2", "column3")) #Needed to create the new tree
+        self.tree=ttk.Treeview(master, column=("column", "column1", "column2")) #Needed to create the new tree
         self.master.title("Work In Progress")
         # Set up the grid configurations below.
         self.master.grid_rowconfigure(0, weight=1)
@@ -21,10 +20,12 @@ class in_progress:
         self.lWorkNum = Label(self.master, text="Work #")
         self.lStatus = Label(self.master, text="Status")
         self.lDaysLeft = Label(self.master, text="Days Until Shipment")
-        self.eSearch = Entry(self.master)
+        self.eSearch = Entry(self.master, width=25)
         
         # Now arrange all of the parts above in a grid.
         self.lTitle.grid(row=0, column=0, sticky=E+W)
+        self.lSearch.grid(row=0, column=1)
+        self.eSearch.grid(row=0, column=2, sticky=E)
 
         # Create the columns and headings below
         self.tree.column("#0", minwidth=0, width=0)

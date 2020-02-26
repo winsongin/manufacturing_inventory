2ccfa8a9ee886c69bfd515f2681ccb2c94fea4b1
jttc98 <jttc98@csu.fullerton.edu>
2020-02-26 09:39:36 -0800

    Removed an unused column from the TreeView

--- inventory_list.py
+++ inventory_list.py
@@ -8,53 +8,40 @@ import mysql.connector
 class inventory:
     def __init__(self,master):
         self.master=master
-        self.tree=ttk.Treeview(master, column=("column", "column1", "column2",
-                                               "column3", "column4")) #Needed to create new columns
-        master.title("Inventory List")
+        self.tree=ttk.Treeview(master, column=("column", "column1",
+                                               "column2", "column3"))#Needed to create new columns
+        self.master.title("Inventory List")
+        # Set up the grid configurations below.
+        self.master.grid_rowconfigure(0, weight=1)
+        self.master.grid_columnconfigure(0, weight=1)
+        
+        # Configure the labels and the entry below
+        self.lTitle = Label(self.master, text="Inventory")
+        self.lSearch = Label(self.master, text="Search")
+        self.lPartName = Label(self.master, text="Part Name")
+        self.lPartNum = Label(self.master, text="Part No.")
+        self.lManufact = Label(self.master, text="Manfacturer")
+        self.lQuantity = Label(self.master, text="Quantity")
+        self.eSearch = Entry(self.master, width=30)
+
+        # Now arrange all of the parts above in a grid.
+        self.lTitle.grid(row=0, column=0, sticky=E+W)
+        self.lSearch.grid(row=0, column=1)
+        self.eSearch.grid(row=0, column=2, sticky=E)
 
         # Create the columns and headings below
         self.tree.column("#0", minwidth=0, width=0)
         self.tree.heading("#1", text="Part Name")
-        self.tree.column("#1", minwidth=0, width=200)
+        self.tree.column("#1", minwidth=0, width=150)
         self.tree.heading("#2", text="Part No.")
         self.tree.column("#2", minwidth=0, width=100)
         self.tree.heading("#3", text="Manufacturer")
-        self.tree.column("#3", minwidth=0, width=150)
+        self.tree.column("#3", minwidth=0, width=125)
         self.tree.heading("#4", text="Quantity")
         self.tree.column("#4", minwidth=0, width=100)
 
         self.tree.configure(height=20)
-        self.tree.grid()
-
-### Create the label widgets below
-##lTitle = tk.Label(win, text = 'Inventory')
-##lSearch = tk.Label(win, text = 'Search')
-##lPartName = tk.Label(win, text = 'Part Name')
-##lPartNum = tk.Label(win, text = 'Part No.')
-##lManufact = tk.Label(win, text = 'Manufacturer')
-##lQuantity = tk.Label(win, text = 'Quantity')
-##
-### Create the entry widget below
-##eSearch = tk.Entry(win)
-##
-### Create the text widgets below
-##tPartName = tk.Text(win, height=30, width=10, state='disabled')
-##tPartNum = tk.Text(win, height=30, width=10, state='disabled')
-##tManufact = tk.Text(win, height=30, width=10, state='disabled')
-##tQuantity = tk.Text(win, height=30, width=10, state='disabled')
-##
-### Now place all the parts in the GUI
-##lTitle.grid(row=0, column=1, pady=3)
-##lSearch.grid(row=0, column=2, pady=3)
-##eSearch.grid(row=0, column=3, pady=3)
-##lPartName.grid(row=1, column=0, pady=3)
-##lPartNum.grid(row=1, column=1, pady=3)
-##lManufact.grid(row=1, column=2, pady=3)
-##lQuantity.grid(row=1, column=3, pady=3)
-##tPartName.grid(row=2, column=0, padx=10)
-##tPartNum.grid(row=2, column=1, padx=10)
-##tManufact.grid(row=2, column=2, padx=10)
-##tQuantity.grid(row=2, column=3, padx=10)
+        self.tree.grid() #Arrange all the TreeView parts in a grid.
 
 if __name__ == "__main__":
     window = Tk()

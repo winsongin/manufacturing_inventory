80a937aaecb585367c002826b4ef3f4ba9f771da
F-montalvo <36055248+F-montalvo@users.noreply.github.com>
2020-02-29 14:10:11 -0800

    Add files via upload

---
+++ login_mechanic.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 22 14:40:00 2020
+
+@author: Francisco
+"""
+import mysql.connector as sql
+
+mydb = sql.connect(
+        host = 'localhost',
+        user = 'root',
+        passwd = 'P@nchito14',
+        database = 'inventory_manager'
+        )
+
+cur = mydb.cursor()
+
+def finddept(p):
+    d = []
+    statement = ("select dept from employees where last_name= '{}'").format(p)
+    cur.execute(statement)
+    results = cur.fetchall()
+    for x in results:
+        ts= ''.join(x)
+        d.append(ts)
+    switch(d[0]) 
+
+def switch(d):
+    print(d)
+        

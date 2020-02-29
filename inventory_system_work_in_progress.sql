e7b6cc1b86ba474efc7d3a3be72930e7fe5f9550
Ralph <rpura951@gmail.com>
2020-02-29 14:13:56 -0800

    Updated to include ETA and Date Received fields

--- inventory_system_work_in_progress.sql
+++ inventory_system_work_in_progress.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: inventory_system
 -- ------------------------------------------------------
--- Server version	8.0.19
+-- Server version	8.0.17
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,6 +26,8 @@ CREATE TABLE `work_in_progress` (
   `wo_number` varchar(7) NOT NULL,
   `status` varchar(10) NOT NULL,
   `company` varchar(45) NOT NULL,
+  `date_recv` date DEFAULT NULL,
+  `eta` date DEFAULT NULL,
   PRIMARY KEY (`wo_number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Shows data for all open work orders';
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -36,7 +38,7 @@ CREATE TABLE `work_in_progress` (
 
 LOCK TABLES `work_in_progress` WRITE;
 /*!40000 ALTER TABLE `work_in_progress` DISABLE KEYS */;
-INSERT INTO `work_in_progress` VALUES ('00001','Receiving','Target'),('00002','Assembly','Best Buy'),('00003','Testing','Walmart'),('00004','Shipping','Microcenter'),('00005','Assembly','Microcenter'),('00006','Testing','Best Buy'),('00007','Testing','Walmart');
+INSERT INTO `work_in_progress` VALUES ('00001','Receiving','Target',NULL,NULL),('00002','Assembly','Best Buy',NULL,NULL),('00003','Testing','Walmart',NULL,NULL),('00004','Shipping','Microcenter',NULL,NULL),('00005','Assembly','Microcenter',NULL,NULL),('00006','Testing','Best Buy',NULL,NULL),('00007','Testing','Walmart',NULL,NULL);
 /*!40000 ALTER TABLE `work_in_progress` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-22 14:01:31
+-- Dump completed on 2020-02-29 14:12:45

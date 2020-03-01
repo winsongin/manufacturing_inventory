89960f137edf55544b94ca50e492c420403a45cb
jttc98 <jttc98@csu.fullerton.edu>
2020-02-29 18:09:32 -0800

    Added Date Received and ETA columns

--- in_progress_list.py
+++ in_progress_list.py
@@ -1,3 +1,4 @@
+
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk
@@ -11,7 +12,7 @@ class in_progress:
     def __init__(self,master):
         self.master=master
         self.tree=ttk.Treeview(master, column=("column",
-        "column1", "column2")) #Needed to create new columns
+        "column1", "column2", "column3", "column4")) #Needed to create new columns
         self.master.title("Work In Progress")
         searchEntry = StringVar()
         self.master.resizable(False, False) #Don't allow users to resize window.
@@ -24,6 +25,8 @@ class in_progress:
         self.lWorkNum = Label(self.master, text="Work #")
         self.lStatus = Label(self.master, text="Status")
         self.lDaysLeft = Label(self.master, text="Days Until Shipment")
+        self.lDateRecv = Label(self.master, text="Date Received")
+        self.lETA = Label(self.master, text="Estimated Time of Arrival")
         self.eSearch = Entry(self.master, textvariable=searchEntry, width=25)
         self.bSearch = Button(self.master, text="Search")
         
@@ -37,9 +40,13 @@ class in_progress:
         self.tree.heading("#1", text="Work #")
         self.tree.column("#1", minwidth=0, width=100)
         self.tree.heading("#2", text="Status")
-        self.tree.column("#2", minwidth=0, width=150)
+        self.tree.column("#2", minwidth=0, width=100)
         self.tree.heading("#3", text="Days Until Shipment")
         self.tree.column("#3", minwidth=0, width=150)
+        self.tree.heading("#4", text="Date Received")
+        self.tree.column("#4", minwidth=0, width=100)
+        self.tree.heading("#5", text="Estimated Time of Arrival")
+        self.tree.column("#5", minwidth=0, width=150)
 
         self.tree.configure(height=20)
         self.tree.grid() #Arrange all the TreeView parts in a grid.

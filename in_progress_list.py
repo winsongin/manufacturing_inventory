6c6ce5d29bc53fc965c9bbcf5bba159f16fc1831
jttc98 <jttc98@csu.fullerton.edu>
2020-03-05 15:48:40 -0800

    Removed the class declarations
    At some point, I do not want to go further in trying to implement the GUI as a class. It would be too overbearing in having to call each member functions and having to perform complex tasks in manipulating the TreeView database.

--- in_progress_list.py
+++ in_progress_list.py
@@ -5,85 +5,81 @@ import mysql.connector
 
 # In Progress List
 
-class in_progress:
-    def __init__(self,master):
-        self.master=master
-        self.tree=ttk.Treeview(master, column=("column", "column1",
-        "column2", "column3", "column4")) #Needed to create new columns
-        self.master.title("Work In Progress")
-        searchEntry = StringVar()
-        self.master.resizable(False, False) #Don't allow users to resize window.
-        # Set up the grid configurations below.
-        self.master.grid_rowconfigure(0, weight=1)
-        self.master.grid_columnconfigure(0, weight=1)
-        
-        # Configure the labels and the entry below
-        self.lTitle = Label(self.master, text="Work In Progress")
-        self.lWorkNum = Label(self.master, text="Work #")
-        self.lStatus = Label(self.master, text="Status")
-        self.lDaysLeft = Label(self.master, text="Days Until Shipment")
-        self.lDateRecv = Label(self.master, text="Date Received")
-        self.lETA = Label(self.master, text="Estimated Time of Arrival")
-        self.eSearch = Entry(self.master, textvariable=searchEntry, width=25)
-        self.bSearch = Button(self.master, text="Search")
-        self.bQuit = Button(self.master, text="Quit", command=self.master.destroy)
-        
-        # Now arrange all of the parts above in a grid.
-        self.lTitle.grid(row=0, column=0, sticky=E+W)
-        self.eSearch.grid(row=0, column=1, sticky=E)
-        self.bSearch.grid(row=0, column=2, sticky=E)
-        self.bQuit.grid(row=1, column=2)
+master = Tk()
+tree=ttk.Treeview(master, column=("column", "column1",
+"column2", "column3", "column4")) #Needed to create new columns
+master.title("Work In Progress")
+searchEntry = StringVar()
+master.resizable(False, False) #Don't allow users to resize window.
+# Set up the grid configurations below.
+master.grid_rowconfigure(0, weight=1)
+master.grid_columnconfigure(0, weight=1)
 
-        # Create the columns and headings below
-        self.tree.column("#0", minwidth=0, width=0)
-        self.tree.heading("#1", text="Work #")
-        self.tree.column("#1", minwidth=0, width=100)
-        self.tree.heading("#2", text="Status")
-        self.tree.column("#2", minwidth=0, width=100)
-        self.tree.heading("#3", text="Days Until Shipment")
-        self.tree.column("#3", minwidth=0, width=150)
-        self.tree.heading("#4", text="Date Received")
-        self.tree.column("#4", minwidth=0, width=100)
-        self.tree.heading("#5", text="Estimated Time of Arrival")
-        self.tree.column("#5", minwidth=0, width=150)
+# Configure the labels and the entry below
+lTitle = Label(master, text="Work In Progress")
+lWorkNum = Label(master, text="Work #")
+lStatus = Label(master, text="Status")
+lDaysLeft = Label(master, text="Days Until Shipment")
+lDateRecv = Label(master, text="Date Received")
+lETA = Label(master, text="Estimated Time of Arrival")
+eSearch = Entry(master, textvariable=searchEntry, width=25)
+bSearch = Button(master, text="Search")
+bQuit = Button(master, text="Quit", command=master.destroy)
 
-        self.tree.configure(height=20)
-        self.tree.grid() #Arrange all the TreeView parts in a grid.
+# Now arrange all of the parts above in a grid.
+lTitle.grid(row=0, column=0, sticky=E+W)
+eSearch.grid(row=0, column=1, sticky=E)
+bSearch.grid(row=0, column=2, sticky=E)
+bQuit.grid(row=1, column=2)
 
-        #Connect to the database if possible.
-        connection=mysql.connector.connect(host="localhost",
-        user="root",password="T1t@n1umus",
-        auth_plugin="mysql_native_password", database="inventory_system")
-        if connection.is_connected():
-            db_Info = connection.get_server_info()
-            print("Connected to MySQL Server version ", db_Info)
-            cursor = connection.cursor()
-            cursor.execute("select database()")
-            records = cursor.fetchone()
-            print("Connected to database called ", records)
+# Create the columns and headings below
+tree.column("#0", minwidth=0, width=0)
+tree.heading("#1", text="Work #")
+tree.column("#1", minwidth=0, width=100)
+tree.heading("#2", text="Status")
+tree.column("#2", minwidth=0, width=100)
+tree.heading("#3", text="Days Until Shipment")
+tree.column("#3", minwidth=0, width=150)
+tree.heading("#4", text="Date Received")
+tree.column("#4", minwidth=0, width=100)
+tree.heading("#5", text="Estimated Time of Arrival")
+tree.column("#5", minwidth=0, width=150)
 
-            #Print all the database records onto the GUI.
-            printAll = "SELECT * FROM work_in_progress"
-            cursor.execute(printAll)
-            records  = cursor.fetchall()
-            for row in records:
-                print("Work Number: ", row[0])
-                print("Status: ", row[1])
-                print("Company: ", row[2])
-                print("Date Received: ", row[3])
-                print("ETA: ", row[4])
+tree.configure(height=20)
+tree.grid() #Arrange all the TreeView parts in a grid.
 
-            counter = 0
-            for row in records:
-                self.tree.insert('', 'end', values=
-                (row[0],row[1],row[2],row[3],row[4]))
-                counter += 1
+#Connect to the database if possible.
+connection=mysql.connector.connect(host="localhost",
+user="root",password="T1t@n1umus",
+auth_plugin="mysql_native_password", database="inventory")
+if connection.is_connected():
+    db_Info = connection.get_server_info()
+    print("Connected to MySQL Server version ", db_Info)
+    cursor = connection.cursor()
+    cursor.execute("select database()")
+    records = cursor.fetchone()
+    print("Connected to database called ", records)
 
-        if (connection.is_connected()):
-            cursor.close()
-            connection.close()
-            print("MySQL connection closed.")
+    #Print all the database records onto the GUI.
+    printAll = "SELECT * FROM work_in_progress"
+    cursor.execute(printAll)
+    records  = cursor.fetchall()
+    for row in records:
+        print("Work Number: ", row[0])
+        print("Status: ", row[1])
+        print("Company: ", row[2])
+        print("Date Received: ", row[3])
+        print("ETA: ", row[4])
+
+    counter = 0
+    for row in records:
+        tree.insert('', 'end', values=
+        (row[0],row[1],row[2],row[3],row[4]))
+        counter += 1
+
+if (connection.is_connected()):
+    cursor.close()
+    connection.close()
+    print("MySQL connection closed.")
                 
-if __name__ == "__main__":
-    window = Tk()
-    main = in_progress(window)
+mainloop()

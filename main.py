ef7c8ffbad023ec9caaaa579e831320a6b495091
Ralph <rpura951@gmail.com>
2020-03-24 21:37:27 -0700

    Functional working for assembly window

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import tkinter
 from tkinter import ttk
 from Login import login
+from assembly import AssyWindow
 from selection import WOWindow
+
 
 def main():
     dept = login()
     print(dept)
     if dept == "Receiving":
-        receiving()
+        return
     elif dept == "Assembly":
-        while(true):
-            workorder = WOWindow(dept)
-            assembly(workorder, workerID)
+        workorder = WOWindow(dept)
+        print(workorder)
+        AssyWindow(workorder, "0002")
     elif dept == "Testing":
-        while(true):
-            workorder = WOWindow(dept)
-            testing(workorder, workerID)
+        while(True):
+            break
+            #workorder = WOWindow(dept)
+            #testing(workorder, workerID)
     elif dept == "Shipping":
-        while(true):
-            workorder = WOWindow(dept)
-            shipping(workorder, workerID)
+        while(True):
+            break
+            #workorder = WOWindow(dept)
+            #shipping(workorder, workerID)
 
 main()

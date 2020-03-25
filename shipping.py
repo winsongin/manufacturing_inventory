d6f6d8b39c612f97b6ee00d5c826269cde2ae603
rpura951 <rpura951@gmail.com>
2020-03-24 23:23:20 -0700

    Updated to automatically pull data

--- shipping.py
+++ shipping.py
@@ -1,17 +1,18 @@
-import pymysql
+import mysql.connector
 from tkinter import *
 import tkinter.messagebox
+import itertools
 
 
 def get_data(wo):
     data = {
-        "host": "39.106.219.251",
+        "host": "localhost",
         "user": "root",
-        "password": "admin",
-        "db": "goods",
+        "password": "Razgriz!949",
+        "db": "inventory_system",
         "charset": "utf8"
     }
-    db = pymysql.connect(**data)
+    db = mysql.connector.connect(**data)
     cursor = db.cursor()
     form_name = "customer"
     select_word = "wo,worker_id,customer,address,qty,tracking,price,order_status"
@@ -21,7 +22,7 @@ def get_data(wo):
     if result is not None:
         result = {
             "wo": result[0],
-            "woriker_id": result[1],
+            "worker_id": result[1],
             "customer": result[2],
             "address": result[3],
             "qty": result[4],
@@ -63,8 +64,10 @@ def execute_update_sql(update_data):
 
 
 class Interface:
-    def __init__(self, interface):
+    def __init__(self, interface, id, wonum):
         self.interface = interface
+        self.id = id
+        self.wonum = wonum
         self._init()
 
     def _init(self):
@@ -75,6 +78,30 @@ class Interface:
         self.tracking = StringVar('')
         self.price = StringVar('')
 
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="Razgriz!949",
+            database="inventory_system"
+        )
+
+        cursor = mydb.cursor()
+
+        wo_query = "SELECT cust_id FROM work_in_progress WHERE wo_number = \"" + self.wonum + "\""
+        cursor.execute(wo_query)
+        cust = cursor.fetchall()
+        customer = cust[0]
+        print(type(customer[0]))
+
+
+        customer_query = "SELECT * FROM customer WHERE cust_id = \"" + customer[0] + "\""
+        cursor.execute(customer_query)
+        test = list(cursor.fetchall())
+        cust_list = list(itertools.chain(*test))
+        cust = cust_list[1]
+        addr = cust_list[2]
+        price = cust_list[3]
+
         Label(self.interface, text='W/O #:').grid(row=0, column=0)
         Label(self.interface, text='Worker ID:').grid(row=1, column=0)
         Label(self.interface, text='Customer:').grid(row=2, column=0)
@@ -83,16 +110,20 @@ class Interface:
         Label(self.interface, text='Tracking Number:').grid(row=5, column=0)
         Label(self.interface, text='Price:').grid(row=6, column=0)
 
-        self.e1 = Entry(self.interface)
-        Label(self.interface, textvariable=self.worker_id).grid(row=1, column=1, padx=10, pady=5)
+        #self.e1 = Entry(self.interface)
+        Label(self.interface, text=self.wonum).grid(row=0, column=1, padx=10, pady=5)
+        Label(self.interface, text=self.id).grid(row=1, column=1, padx=10, pady=5)
         Label(self.interface, textvariable=self.customer).grid(row=2, column=1, padx=10, pady=5)
+        Label(self.interface, text=cust).grid(row=2, column=2)
         Label(self.interface, textvariable=self.address).grid(row=3, column=1, padx=10, pady=5)
+        Label(self.interface, text=addr).grid(row=3, column=2, padx=10, pady=5)
         Label(self.interface, textvariable=self.qty).grid(row=4, column=1, padx=10, pady=5)
         self.tracking = Entry(self.interface)
         self.tracking.grid(row=5, column=1, padx=10, pady=5)
         Label(self.interface, textvariable=self.price).grid(row=6, column=1, padx=10, pady=5)
+        Label(self.interface, text=price).grid(row=6, column=2, padx=10, pady=5)
 
-        self.e1.grid(row=0, column=1, padx=10, pady=5)
+        #self.e1.grid(row=0, column=1, padx=10, pady=5)
 
         Button(self.interface, text='Print', width=10, command=self.show).grid(row=7, column=0, sticky=W, padx=10, pady=5)
         Button(self.interface, text='Submit', width=10, command=self.submit).grid(row=8, column=0, sticky=W, padx=10, pady=5)
@@ -155,12 +186,12 @@ class Interface:
         清空所有Entry
         """
         self.e1.delete(0, END)
-        self.worker_id.set("")
-        self.customer.set("")
-        self.address.set("")
-        self.qty.set("")
+        self.worker_id.set("1")
+        self.customer.set("2")
+        self.address.set("3")
+        self.qty.set("4")
         self.tracking.delete(0, END)
-        self.price.set("")
+        self.price.set("5")
 
     def set_value(self, data):
         """
@@ -190,5 +221,5 @@ if __name__ == "__main__":
     interface.configure()
     interface.title("Inventory")
     interface.geometry("600x440")
-    Interface(interface)
+    Interface(interface, "0003", "6")
     interface.mainloop()

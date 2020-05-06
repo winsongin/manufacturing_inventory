ddfb62ab1ee7c22b63bc4a7142d8f491f1f13d59
Omar Ramirez <47376992+oramirez1397@users.noreply.github.com>
2020-05-05 18:55:19 -0700

    Updated with new layout.

--- testing.py
+++ testing.py
@@ -1,6 +1,8 @@
-from tkinter import * #importing the tkinter class.
-import tkinter.messagebox #Able to create pop-up Messages.
+from tkinter import *
+from tkinter.ttk import *
+import tkinter.messagebox
 import mysql.connector
+import itertools
 
 import assembly_class
 import login
@@ -11,82 +13,81 @@ import accounting
 import admin
 import selection
 
-#class Decleration
+
 class TestingWindow:
-    def __init__(self, master, wo, emp_id):
+    def __init__(self, master, emp_id):
         self.master = master
-        self.wo = wo
-        self.emp_id = emp_id
+        self.wo = ""
+        self.wonum = "Work Order: " + self.wo
+        self.emp_id = "Worker ID: " + emp_id
         self.mydb = mysql.connector.connect(
             host="localhost",
             user="root",
             passwd="Omar131997",
             database="inventory_system"
         )
+        self.cursor = self.mydb.cursor(buffered=True)
+        self.testWindow()
 
-        self.FileMenu = Menu(master)
-        self.master.config(menu= self.FileMenu)
+    def testWindow(self):
+
+        # Instantiates style for ttk buttons. Please use this for your buttons
+        style = Style()
+
+        self.FileMenu = Menu(self.master)
+        self.master.config(menu=self.FileMenu)
 
         self.subMenu = Menu(self.FileMenu)
         self.subMenu2 = Menu(self.FileMenu)
         self.FileMenu.add_cascade(label="File", menu=self.subMenu)
         self.subMenu.add_command(label="Exit", command=self.master.destroy)
-        self.subMenu.add_command(label = "Logout", command=self.Logoff)
-        self.FileMenu.add_cascade(label="Edit", menu=self.subMenu2)
+        self.subMenu.add_command(label="Logout", command=self.Logoff)
 
         # creating labels here:
-        self.testing_label = Label(master, text="TESTING PROCESS", relief="solid", width=18, font=("arial", 25, "bold"))
-
-        # Displaying work order number.
-        self.orderNumber_label = Label(master, text="Work Order Number: ", bg="yellow", font=("arial", 15, "bold"))
-        self.work_number = StringVar()
-        self.number = Label(master, text=self.wo, font=("arial", 15, "bold"))
-        self.number.place(x=225, y=80)
+        self.testing_label = Label(self.master, text="TESTING PROCESS", relief="solid", width=18, font=("arial", 25, "bold"))
 
         # displaying worker ID:
-        self.workerID_label = Label(master, text="Worker ID: ", bg="yellow", font=("arial", 15, "bold"))
-        self.id = StringVar()
-        self.id_label = Label(master, text=self.emp_id, font=("arial", 15, "bold"))
-        self.id_label.place(x=225, y=110)
+        self.workerID_label = Label(self.master, text=self.emp_id, font=("arial", 15, "bold"))
+        self.workerID_label.place(x=225, y=110)
 
         # displaying Order Status:
-        self.status_label = Label(master, text="Order Status: ", bg="yellow", font=("arial", 15, "bold"))
+        self.status_label = Label(self.master, text="Order Status: ", font=("arial", 15, "bold"))
         self.stat = StringVar()
         self.orderStatusEntry = Label(self.master, text="Testing", font=("arial", 15, "bold"))
 
-        self.test1_label = Label(master, text="Test 1: Speed ", bg="red", font=("arial", 13, "bold"))
-        self.test2_label = Label(master, text="Test 2: Durability ", bg="red", font=("arial", 13, "bold"))
-        self.test3_label = Label(master, text="Test 3: Battery Efficiency  ", bg="red", font=("arial", 13, "bold"))
-        self.test4_label = Label(master, text="Test 4: Remote Signal Range ", bg="red", font=("arial", 13, "bold"))
-        self.parameters_label = Label(master, text="Parameters", relief="solid", width=10, font=("arial", 12, "bold"))
+        self.test1_label = Label(self.master, text="Test 1: Speed ", font=("arial", 13, "bold"))
+        self.test2_label = Label(self.master, text="Test 2: Durability ", font=("arial", 13, "bold"))
+        self.test3_label = Label(self.master, text="Test 3: Battery Efficiency  ", font=("arial", 13, "bold"))
+        self.test4_label = Label(self.master, text="Test 4: Remote Signal Range ", font=("arial", 13, "bold"))
+        self.parameters_label = Label(self.master, text="Parameters", relief="solid", width=10, font=("arial", 12, "bold"))
 
-        self.p1 = Label(master, text="[5-12 MPH]", font=("arial", 12, "bold"))
-        self.p2 = Label(master, text="[98% or >]", font=("arial", 12, "bold"))
-        self.p3 = Label(master, text="[2-4 Hours]", font=("arial", 12, "bold"))
-        self.p4 = Label(master, text="[98% or >]", font=("arial", 12, "bold"))
+        self.p1 = Label(self.master, text="[5-12 MPH]", font=("arial", 12, "bold"))
+        self.p2 = Label(self.master, text="[98% or >]", font=("arial", 12, "bold"))
+        self.p3 = Label(self.master, text="[2-4 Hours]", font=("arial", 12, "bold"))
+        self.p4 = Label(self.master, text="[98% or >]", font=("arial", 12, "bold"))
 
         # Creating buttons here:
-        self.enter_button = Button(master, text="Enter", bg="blue", fg="white", command=lambda: self.onClick(),
-                                   font=("arial", 13, "bold"))  # Command is binding to fuction.
-        self.reset_button = Button(master, text="Reset", command=self.reset, bg="blue", fg="white",
-                                   font=("arial", 13, "bold"))
+        style.configure('C.TButton', padding=0, font=("arial", 12), background='gray',
+                        foreground='Green')
+
+        self.enter_button = Button(self.master, text="Enter", command=lambda: self.onClick(),  style='C.TButton')  # Command is binding to fuction.
+        self.reset_button = Button(self.master, text="Reset", command=self.reset, style='C.TButton')
 
         # User input
         self.E3 = IntVar()
-        self.entry3 = Entry(master, textvariable=self.E3)
+        self.entry3 = Entry(self.master, textvariable=self.E3)
         self.E4 = IntVar()
-        self.entry4 = Entry(master, textvariable=self.E4)
+        self.entry4 = Entry(self.master, textvariable=self.E4)
         self.E5 = IntVar()
-        self.entry5 = Entry(master, textvariable=self.E5)
+        self.entry5 = Entry(self.master, textvariable=self.E5)
         self.E6 = IntVar()
-        self.entry6 = Entry(master, textvariable=self.E6)
+        self.entry6 = Entry(self.master, textvariable=self.E6)
 
         # Placing all Labels and Buttons on to window.
         self.testing_label.place(x=130, y=10)
-        self.orderNumber_label.place(x=10, y=80)
         self.workerID_label.place(x=10, y=110)
         self.status_label.place(x=10, y=140)
-        self.orderStatusEntry.place(x=225, y=140)
+        self.orderStatusEntry.place(x=150, y=140)
         self.parameters_label.place(x=480, y=190)
 
         # placing items here on master
@@ -107,7 +108,44 @@ class TestingWindow:
         self.entry5.place(x=310, y=280)
         self.entry6.place(x=310, y=310)
 
-    # Logof function will let user logout.
+        #Selection Grid
+        tree = Treeview(self.master, column=("column", "column1", "column2", "column3", "column4"))
+        searchQuery = StringVar()
+        selection = StringVar()
+
+        load_button = Button(self.master, text="Select Work Order", command=lambda: self.select(selection.get()), style='C.TButton')
+        selection_entry = Entry(self.master, width=15, textvariable=selection)
+
+        selection_entry.place(x=500, y=8)
+        load_button.place(x=500, y=30)
+
+        tree.column("#0", minwidth=0, width=0, stretch=False)
+        tree.heading("#1", text="Work Order", command=lambda: self.sort_column(tree, 0, "Assembly", False))
+        tree.column("#1", minwidth=0, width=100, stretch=False)
+        tree.heading("#2", text="Department", command=lambda: self.sort_column(tree, 1, "Assembly", False))
+        tree.column("#2", minwidth=0, width=100, stretch=False)
+        tree.heading("#3", text="Customer", command=lambda: self.sort_column(tree, 2, "Assembly", False))
+        tree.column("#3", minwidth=0, width=150, stretch=False)
+        tree.heading("#4", text="Received Date", command=lambda: self.sort_column(tree, 3, "Assembly", False))
+        tree.column("#4", minwidth=0, width=150, stretch=False)
+        tree.heading("#5", text="Estimated Ship", command=lambda: self.sort_column(tree, 4, "Assembly", False))
+        tree.column("#5", minwidth=0, width=150, stretch=False)
+
+        tree.configure(height=5)
+        tree.place(x=7, y=370) # You may need to increase y's value to fit your form
+
+        printAll = "SELECT * FROM work_in_progress WHERE status = \"" + "TestingWindow" + "\"" # CHANGE "ASSEMBLY" TO YOUR DEPARTMENT
+        cursor = self.mydb.cursor()
+        cursor.execute(printAll)
+        records = cursor.fetchall()
+
+        counter = 0
+        for row in records:
+            tree.insert('', 'end', values=
+            (row[0], row[1], row[2], row[3], row[4]))
+            counter += 1
+
+    # Logout Function
     def Logoff(self):
         answer = tkinter.messagebox.askquestion("Logout", "Are you sure you want to logout? ")
 
@@ -128,22 +166,21 @@ class TestingWindow:
             elif login1.dept == "Assembly":
                 workorder = selection.WOWindow(login1.dept)
                 root = Tk()
-                root.geometry("600x500")
+                root.geometry("665x340")
                 root.title("Assembly")
-                app = assembly_class.assembly(root, "0002", workorder)
+                app = assembly_class.Assembly(root, "0002")
                 root.mainloop()
             elif login1.dept == "Testing":
                 workorder = selection.WOWindow(login1.dept)
                 root = Tk()
-                root.geometry("600x500")
+                root.geometry("680x500")
                 root.title("Testing")
-                app = testing.TestingWindow(root, workorder, "0003")
+                app = testing.TestingWindow(root, "0003")
                 root.mainloop()
             elif login1.dept == "Shipping":
                 workorder = selection.WOWindow(login1.dept)
                 root = Tk()
-                app = shipping_connector.Interface(root, workorder, "0004")
-                shipping_connector(workorder, workerID)
+                shipping_connector.Interface(root, workorder, "0004")
                 root.mainloop()
             elif login1.dept == "Accounting":
                 root = Tk()
@@ -160,8 +197,7 @@ class TestingWindow:
                 root.configure(bg="light gray")
                 admin1 = admin.Admin(root)
                 root.mainloop()
-
-
+    #function will check if pass or fail.
     def onClick(self):
         answer = tkinter.messagebox.askquestion("RESULT", "Are these tests correct? ")
         cursor = self.mydb.cursor()
@@ -176,8 +212,8 @@ class TestingWindow:
             tkinter.messagebox.showinfo("RESULT", "TESTS FAILED!")
             cursor.execute("UPDATE work_in_progress SET status = 'Assembly' ")
             self.mydb.commit()
-            
-    #gets status of order
+
+    # gets status of order
     def getStatus(self):
         cursor3 = self.mydb.cursor()
         statement = "SELECT status  FROM work_in_progress WHERE status = '{}' "
@@ -192,11 +228,53 @@ class TestingWindow:
         self.E5.set(' ')
         self.E6.set(' ')
 
+    def sort_column(self, tree, col, department, reverse):
+        tree.delete(*tree.get_children())
+        db_Info = self.mydb.get_server_info()
+        cursor = self.mydb.cursor()
+        cursor.execute("select database()")
+        records = cursor.fetchone()
+
+        if (col == 0):
+            querySort = "SELECT * FROM work_in_progress WHERE status = \"" + department + "\" ORDER BY wo_number"
+        elif (col == 1):
+            querySort = "SELECT * FROM work_in_progress WHERE status = \"" + department + "\" ORDER BY status"
+        elif (col == 2):
+            querySort = "SELECT * FROM work_in_progress WHERE status = \"" + department + "\" ORDER BY date_recv"
+        elif (col == 3):
+            querySort = "SELECT * FROM work_in_progress WHERE status = \"" + department + "\" ORDER BY eta"
+        elif (col == 4):
+            querySort = "SELECT * FROM work_in_progress WHERE status = \"" + department + "\" ORDER BY cust_id"
+
+        cursor.execute(querySort)
+        records = cursor.fetchall()
+        for row in records:
+            print("Work Number: ", row[0])
+            print("Status: ", row[1])
+            print("Date Received: ", row[2])
+            print("ETA: ", row[3])
+            print("Customer ID: ", row[4])
+
+        counter = 0
+        for row in records:
+            tree.insert('', 'end', values=
+            (row[0], row[1], row[2], row[3], row[4]))
+            counter += 1
+
+        cursor.close()
+        self.mydb.close()
+        print("MySQL connection closed.")
+
+    def select(self, wo):
+        self.wonum = "Work Order: " + wo
+        self.wo = wo
+        number = Label(self.master, text=self.wonum, font=("arial", 15, "bold"))
+        number.place(x=10, y=80)
+
 
 if __name__ == "__main__":
-    master = Tk()
-    master.geometry("600x500")  # Resizing window
-    master.configure(bg="light gray")
-    master.title("Testing")  # giving window a title
-    window = TestingWindow(master,"1", "0003")
-    master.mainloop()
+    root = Tk()
+    root.geometry("680x500")
+    root.title("Testing")
+    app = TestingWindow(root, "0001")
+    root.mainloop()
